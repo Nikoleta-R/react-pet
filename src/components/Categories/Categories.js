4bbe97f1812107fd83907a689b6999718586ec30
Nikoleta-R <raychinova_nikol@gmail.com>
2021-03-28 20:20:08 +0300

    add details

--- src/components/Categories/Categories.js
+++ src/components/Categories/Categories.js
@@ -1,8 +1,9 @@
 
 import React, { useEffect, useState } from 'react'
-import CategoryNavigation from './CategoryNavigation'
-import Pet from '../Pet'
-import { Component } from 'react';
+import CategoryNavigation from './CategoryNavigation';
+import * as petsServices from '../../services/petsServices';
+import Pet from '../Pet';
+
 
 
 const Categories = ({
@@ -13,11 +14,8 @@ const Categories = ({
     
    const category = match.params.category ? match.params.category :'';
 
-   
-   
     useEffect(()=>{
-        fetch(`http://localhost:5002/categories/${category}`)
-        .then(res => res.json())
+        petsServices.getAll(category)
         .then(res=>setPets(res));
      
 
